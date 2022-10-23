275e79de428514b384a55f11928a979b6fad50a3
Aviram Hassan <aviramyhassan@gmail.com>
2022-10-23 15:10:16 +0300

    first version?

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # macos-sip
+
+This is a utility crate for [mirrord](https://github.com/metalbear-co/mirrord) to help it inject `mirrord-layer` into macOS' SIP protected binaries.
+It is released as open source, licensed under MIT, and is not affiliated with Apple in any way.
+This crate isn't developed for security/breach purposes, but to help developers debug their applications.
+We don't publish it to crates.io, but you can still use it by adding the following to your `Cargo.toml`:
+
+```toml
+[dependencies]
+macos-sip = { git = "https://github.com/metalbear-co/macos-sip" }
+```
+
+In case there will be a demand we'll start publishing it also to crates.io.
+
+
+## CHANGELOG
+[Here](./CHANGELOG.md)
+
+## License
+[MIT](./LICENSE)
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 mod codesign;
 mod error;
 
-use std::ops::Range;
-use std::os::macos::fs::MetadataExt;
+use std::{
+    fs::Permissions,
+    os::{macos::fs::MetadataExt, unix::prelude::PermissionsExt},
+};
 
 use object::{
     macho::{FatHeader, MachHeader32, MachHeader64, CPU_TYPE_X86_64},
@@ -12,123 +14,78 @@ use object::{
 
 use crate::error::{Result, SipError};
 
+fn is_fat_x64_arch(arch: &&impl FatArch) -> bool {
+    matches!(arch.architecture(), Architecture::X86_64)
+}
+
 struct BinaryInfo {
     offset: usize,
     size: usize,
 }
 
-fn is_fat_x64_arch(arch: &&impl FatArch) -> bool {
-    matches!(arch.architecture(), Architecture::X86_64)
-}
+impl BinaryInfo {
+    fn new(offset: usize, size: usize) -> Self {
+        Self { offset, size }
+    }
 
-/// Gets x64 binary offset from a Mach-O fat binary or returns 0 if is x64 macho binary.
-fn get_x64_binary_offset(file: &[u8]) -> Result<usize> {
-    match FileKind::parse(file)? {
-        FileKind::MachO32 => {
-            let header: &MachHeader32<Endianness> = MachHeader::parse(file, 0).map_err(|_| {
-                SipError::UnsupportedFileFormat("MachO 32 file parsing failed".to_string())
-            })?;
-            if header.cputype(Endianness::default()) == CPU_TYPE_X86_64 {
-                Ok(0)
-            } else {
-                Err(SipError::NoX64Arch("MachO file is not x64".to_string()))
+    /// Gets x64 binary offset from a Mach-O fat binary or returns 0 if is x64 macho binary.
+    fn from_object_bytes(bytes: &[u8]) -> Result<Self> {
+        match FileKind::parse(bytes)? {
+            FileKind::MachO32 => {
+                let header: &MachHeader32<Endianness> =
+                    MachHeader::parse(bytes, 0).map_err(|_| {
+                        SipError::UnsupportedFileFormat("MachO 32 file parsing failed".to_string())
+                    })?;
+                if header.cputype(Endianness::default()) == CPU_TYPE_X86_64 {
+                    Ok(Self::new(0, bytes.len()))
+                } else {
+                    Err(SipError::NoX64Arch("MachO file is not x64".to_string()))
+                }
             }
-        }
-        FileKind::MachO64 => {
-            let header: &MachHeader64<Endianness> = MachHeader::parse(file, 0).map_err(|_| {
-                SipError::UnsupportedFileFormat("MachO 64 file parsing failed".to_string())
-            })?;
-            if header.cputype(Endianness::default()) == CPU_TYPE_X86_64 {
-                Ok(0)
-            } else {
-                Err(SipError::NoX64Arch("MachO file is not x64".to_string()))
+            FileKind::MachO64 => {
+                let header: &MachHeader64<Endianness> =
+                    MachHeader::parse(bytes, 0).map_err(|_| {
+                        SipError::UnsupportedFileFormat("MachO 64 file parsing failed".to_string())
+                    })?;
+                if header.cputype(Endianness::default()) == CPU_TYPE_X86_64 {
+                    Ok(Self::new(0, bytes.len()))
+                } else {
+                    Err(SipError::NoX64Arch("MachO file is not x64".to_string()))
+                }
             }
+            FileKind::MachOFat32 => FatHeader::parse_arch32(bytes)
+                .map_err(|_| SipError::UnsupportedFileFormat("FatMach-O 32-bit".to_string()))?
+                .iter()
+                .find(is_fat_x64_arch)
+                .map(|arch| Self::new(arch.offset() as usize, arch.size() as usize))
+                .ok_or_else(|| {
+                    SipError::NoX64Arch("Couldn't find x64 arch in fat file".to_string())
+                }),
+            FileKind::MachOFat64 => FatHeader::parse_arch64(bytes)
+                .map_err(|_| SipError::UnsupportedFileFormat("Mach-O 32-bit".to_string()))?
+                .iter()
+                .find(is_fat_x64_arch)
+                .map(|arch| Self::new(arch.offset() as usize, arch.size() as usize))
+                .ok_or_else(|| {
+                    SipError::NoX64Arch("Couldn't find x64 arch in fat file".to_string())
+                }),
+            other => Err(SipError::UnsupportedFileFormat(
+                format!("{:?}", other).to_string(),
+            )),
         }
-        FileKind::MachOFat32 => FatHeader::parse_arch32(file)
-            .map_err(|_| SipError::UnsupportedFileFormat("FatMach-O 32-bit".to_string()))?
-            .iter()
-            .find(is_fat_x64_arch)
-            .map(|arch| arch.offset() as usize)
-            .ok_or_else(|| SipError::NoX64Arch("Couldn't find x64 arch in fat file".to_string())),
-        FileKind::MachOFat64 => FatHeader::parse_arch64(file)
-            .map_err(|_| SipError::UnsupportedFileFormat("Mach-O 32-bit".to_string()))?
-            .iter()
-            .find(is_fat_x64_arch)
-            .map(|arch| arch.offset() as usize)
-            .ok_or_else(|| SipError::NoX64Arch("Couldn't find x64 arch in fat file".to_string())),
-        other => Err(SipError::UnsupportedFileFormat(
-            format!("{:?}", other).to_string(),
-        )),
     }
 }
 
 /// Patches a binary to disable SIP.
-/// Right now it extracts
+/// Right now it extracts x64 binary from fat/MachO binary and patches it.
 pub fn patch_binary(path: &str, output: &str) -> Result<()> {
-    let mut data = std::fs::read(path)?;
-    let (header, _): (&FatHeader, _) =
-        object::from_bytes(&data[..std::mem::size_of::<FatHeader>()]).unwrap();
-    let magic = header.magic.get(BigEndian);
-    if magic != FAT_MAGIC {
-        return Err(anyhow!("invalid magic {magic:?}"));
-    }
-    let arch_count = header.nfat_arch.get(BigEndian) as usize;
-    let archs_range = Range {
-        start: std::mem::size_of::<FatHeader>(),
-        end: std::mem::size_of::<FatHeader>() + arch_count * std::mem::size_of::<FatArch32>(),
-    };
-    let (archs, _): (&[FatArch32], _) =
-        object::slice_from_bytes(&data[archs_range], arch_count).unwrap();
-    // let mut arch_offset = 0;
-    // let mut arch_size = 0;
-    let range = Range {
-        start: archs[0].offset.get(BigEndian) as usize,
-        end: archs[0].offset.get(BigEndian) as usize + archs[0].size.get(BigEndian) as usize,
-    };
-    let x64 = &data[range];
+    let data = std::fs::read(path)?;
+    let binary_info = BinaryInfo::from_object_bytes(&data)?;
 
-    // for arch in archs {
-    //     let cpu_type = arch.cputype.get(BigEndian);
-    //     let cpu_subtype = arch.cpusubtype.get(BigEndian);
-    //     println!("test: {:?}", cpu_type & CPU_TYPE_ARM64);
-    //     if cpu_type == CPU_TYPE_ARM64 && (cpu_subtype & CPU_SUBTYPE_ARM64E) > 0 {
-    //         let val = CPU_SUBTYPE_ARM64_ALL; // CPU_SUBTYPE_ARM64_ALL; // (cpu_subtype ^ CPU_SUBTYPE_ARM64E) ^ ;
-    //         arch.cpusubtype.set(BigEndian, val);
-    //         arch_offset = arch.offset.get(BigEndian) as usize;
-    //         arch_size = arch.size.get(BigEndian) as usize;
-    //         println!("patch1 {val:?}");
-    //     } else {
-    //         println!("{cpu_type:?} {cpu_subtype:?}");
-
-    //     }
-
-    // }
-    // {
-    //     let hash = md5::compute(&data);
-    //     println!("second: {:x}", hash);
-    // }
-    // let range = Range {
-    //     start: arch_offset,
-    //     end: arch_offset + arch_size,
-    // };
-    // let (arch_header, _): (&mut MachHeader64<LittleEndian>, _) = object::from_bytes_mut(&mut data[range]).unwrap();
-    // let cpu_type = arch_header.cputype.get(LittleEndian);
-    // let cpu_subtype = arch_header.cpusubtype.get(LittleEndian);
-    // println!("magic {:?}", arch_header.magic.get(BigEndian));
-    // if cpu_type == CPU_TYPE_ARM64 && (cpu_subtype & CPU_SUBTYPE_ARM64E) > 0 {
-    //     println!("patch2");
-    //     let val = CPU_SUBTYPE_ARM64_ALL; // (cpu_subtype ^ CPU_SUBTYPE_ARM64E) ^ CPU_SUBTYPE_PTRAUTH_ABI;
-    //     arch_header.cpusubtype.set(LittleEndian, val);
-    // } else {
-    //     println!("{cpu_type:?} {cpu_subtype:?}");
-    // }
-    // {
-    //     let hash = md5::compute(&data);
-    //     println!("last: {:x}", hash);
-    // }
-    std::fs::write(output, x64).unwrap();
-    codesign::sign(output).unwrap();
-    Ok(())
+    let x64_binary = &data[binary_info.offset..binary_info.offset + binary_info.size];
+    std::fs::write(output, x64_binary)?;
+    std::fs::set_permissions(output, Permissions::from_mode(0o755))?;
+    codesign::sign(output)
 }
 
 const SF_RESTRICTED: u32 = 0x00080000; // entitlement required for writing, from stat.h (macos)
@@ -142,7 +99,10 @@ pub fn is_sip(path: &str) -> Result<bool> {
 #[cfg(test)]
 mod tests {
 
+    use std::io::Write;
+
     use super::*;
+    use tempfile::tempfile;
 
     #[test]
     fn is_sip_true() {
@@ -151,13 +111,30 @@ mod tests {
 
     #[test]
     fn is_sip_false() {
-        std::fs::copy("/bin/ls", "/tmp/ls_mirrord_test").unwrap();
-        assert!(!is_sip("/tmp/ls_mirrord_test").unwrap());
+        let mut f = tempfile::NamedTempFile::new().unwrap();
+        let data = std::fs::read("/bin/ls").unwrap();
+        f.write(&data).unwrap();
+        f.flush().unwrap();
+        assert!(!is_sip(f.path().to_str().unwrap()).unwrap());
     }
 
     #[test]
     fn is_sip_notfound() {
         let err = is_sip("/donald/duck/was/a/duck/not/a/quack/a/duck").unwrap_err();
         assert!(err.to_string().contains("No such file or directory"));
+    }
+
+    #[test]
+    fn patch_binary_fat() {
+        let path = "/bin/ls";
+        let output = "/tmp/ls_mirrord_test";
+        patch_binary(path, output).unwrap();
+        assert!(!is_sip(output).unwrap());
+        // Check DYLD_* features work on it:
+        let output = std::process::Command::new(output)
+            .env("DYLD_PRINT_LIBRARIES", "1")
+            .output()
+            .unwrap();
+        assert!(String::from_utf8_lossy(&output.stderr).contains("libsystem_kernel.dylib"));
     }
 }
